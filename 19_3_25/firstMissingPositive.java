b1cbdcaab136b350308f24622c015629e75e8cb2
YADHAV <yadhavdhv@gmail.com>
2025-03-19 21:52:23 +0530

    Add files via upload

---
+++ 19_3_25/firstMissingPositive.java
@@ -0,0 +1,26 @@
+class firstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+               int n = nums.length;
+
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = n + 1;  
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int val = Math.abs(nums[i]);
+            if (val <= n) {
+                nums[val - 1] = -Math.abs(nums[val - 1]);  
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;  
+            }
+        }
+        return n + 1;
+    }
+}
